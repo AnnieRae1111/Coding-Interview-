b96aa00ba5ae89b2dc8b811986d9292ca587da4b
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-06-02 08:13:03 -0600

    stacks and queues

---
+++ Stacks/stacks.js
@@ -0,0 +1,12 @@
+//Stacks
+//like browser history
+
+//google
+//udemy.com
+//youtube
+
+//since google is the first item, it is place on the stack
+//stack woudl work like this:
+//youtube is fisrt because that is the last thing we visited
+//udemy.com
+//google
